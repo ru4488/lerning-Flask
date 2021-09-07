471f0f84f12513a7beaac4d995d0530ef815c092
Dormidontov Dmitry <44_88@mail.ru>
2021-09-07 22:26:36 +0300

    Добавил поддежку базы данных, теперь новости загружаются из базы данных

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask  , render_template
 
-from webapp.model import db 
-from webapp.python_org_news import get_python_news
+from webapp.model import db , News
+
 from webapp.weather import weather_by_city
 
 
@@ -14,7 +14,7 @@ def create_app():
     def index():
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'] , app.config['NUMBER_OF_WEATHER_DAYS'])
         tittle = "Новости Python"
-        news_list =  get_python_news()
+        news_list =  News.query.order_by(News.published.desc()).all()
 
         return render_template("index.html ", page_tittle = tittle , weather = weather , news_list = news_list )
 
