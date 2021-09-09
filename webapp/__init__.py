46c2b00bd134769ff863cfb42bece7999ee55d4f
Dormidontov Dmitry <44_88@mail.ru>
2021-09-09 22:20:03 +0300

    Сделал разметку страницы для ввода логина и пароля

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask  , render_template
 
 from webapp.model import db , News
-
+from webapp.forms import LoginForm
 from webapp.weather import weather_by_city
 
 
@@ -15,8 +15,13 @@ def create_app():
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'] , app.config['NUMBER_OF_WEATHER_DAYS'])
         tittle = "Новости Python"
         news_list =  News.query.order_by(News.published.desc()).all()
-
         return render_template("index.html ", page_tittle = tittle , weather = weather , news_list = news_list )
+
+    @app.route('/login')
+    def login():
+        title = "Авотризация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
 
     return app
 
