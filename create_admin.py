376dc3ac61530e0b5d13805aa2bf4360a18512eb
Dormidontov Dmitry <44_88@mail.ru>
2021-09-10 23:12:21 +0300

    ошибка при записи нового пользователя

---
+++ create_admin.py
@@ -0,0 +1,30 @@
+from getpass import getpass
+import sys
+
+from webapp import create_app 
+from webapp.model import User , db  
+
+
+app = create_app()
+
+with app.app_context():
+
+    username = input("Введите имя: ")
+
+    if User.query.filter(User.username == username).count():
+        print('Такой пользователь уже есть')
+        sys.exit(0)
+
+    password1 = getpass("Введите пароль: ")
+    password2 = getpass("Повторите пароль: ")
+
+    if not password1 == password2:
+        print("Пароли не совпадают, попробуй еще раз, дружок")
+        sys.exit(0)
+
+    new_user = User(username=username , role = "ADминушка")
+    new_user.set_password(password1)
+
+    db.session.add(new_user)
+    db.session.commit()
+    print("Создан пользователь с id {}".format(new_user.id))
--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,6 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy 
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -10,5 +12,21 @@ class News(db.Model):
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return "<News {} {}>".format(self.title , self.url)
+
+class User(db.Model , UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True ,  unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10) , index=True)
+    
+    def set_password(self , password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+    def __repr__(self):
+        return "<User {} >".format(self.username)
