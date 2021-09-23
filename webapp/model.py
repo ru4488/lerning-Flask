1581e43c7a698b9478a8dee5cbdfa556c6cb92e2
Dormidontov Dmitry <44_88@mail.ru>
2021-09-23 22:59:03 +0300

    Сделана проверка на администратора, не добавляются новости в базу данных

--- webapp/model.py
+++ webapp/model.py
@@ -27,6 +27,11 @@ class User(db.Model , UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
 
     def __repr__(self):
-        return "<User {} >".format(self.username)
+        return "<User {} , id {}>".format(self.username , self.id)
+
+
--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -2,7 +2,7 @@
 from datetime import datetime 
 import requests
 from bs4 import BeautifulSoup
-from webapp.model import db , News
+from model import db , News
 
 def get_html(url):
     try:
@@ -15,6 +15,7 @@ def get_html(url):
 
 def get_python_news():
     html = get_html("https://www.python.org/blogs/")
+
     if html:
         soup = BeautifulSoup(html , "html.parser")
         all_news = soup.find("ul" , class_="list-recent-posts menu").find_all("li")
@@ -24,16 +25,25 @@ def get_python_news():
             title = news.find("a").text
             url = news.find("a")["href"]
             published = news.find("time").text
+
             try:
                 published = datetime.strptime(published , "%Y-%m-%d")
             except(ValueError):
                 published = datetime.now()
+
             save_news(title , url , published)
 
 
 def save_news(title , url , published):
+
     news_exists = News.query.filter(News.url == url).count()
+    
     if not news_exists:
+        
         new_news = News(title = title , url = url, published = published)
         db.session.add(new_news)
         db.session.commit()
+
+
+if __name__ == "__main__":
+    get_python_news()
