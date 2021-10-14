d9a779ac71cee46f08fdcfa9d4434a13e319cb2c
Dormidontov Dmitry <44_88@mail.ru>
2021-10-14 22:12:18 +0300

    программа сообщает ошибку при регистрации уже занятого аккаунта или email

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField , StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired , Email , EqualTo
+from wtforms.validators import DataRequired , Email , EqualTo , ValidationError
+
+from webapp.user.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()] , render_kw={"class": "form-control"})
@@ -15,3 +17,15 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()] , render_kw={"class": "form-control"})
     password2 = PasswordField('Повторите пароль', validators=[DataRequired() , EqualTo("password")] , render_kw={"class": "form-control"})
     submit = SubmitField('Отправить' , render_kw={"class": "btn btn-info"})
+
+
+
+    def validate_username(self , username):
+        users_count = User.query.filter_by(username = username.data).count()
+        if users_count > 0:
+            raise ValidationError("Такой пользователь уже есть, придумай другое имя, или вспоминай пароль")
+
+    def validate_email(self , email):
+        users_count = User.query.filter_by(email = email.data).count()
+        if users_count > 0:
+            raise ValidationError("Такой email уже есть, придумай другое имя, или вспоминай пароль")
