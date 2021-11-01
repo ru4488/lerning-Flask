12f3ab3a09bfc60a437684589c831afbc36098ed
Dormidontov Dmitry <44_88@mail.ru>
2021-11-01 23:32:44 +0300

    закончил курс, добавлены проверки и возможность оставлять комментарии

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,6 +4,8 @@ from flask_login import current_user ,  login_user ,  logout_user
 from webapp.db import db
 from webapp.user.forms import LoginForm , RegistrationForm
 from webapp.user.models import User 
+from webapp.utils import get_redirect_target
+
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
 
@@ -11,7 +13,7 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 def login():
 
     if current_user.is_authenticated:
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target())
     title = "Авотризация"
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
@@ -25,7 +27,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user , remember=form.remember_me.data)
             flash('Вы вошли на сайт')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())
     
     flash('Неправильное имя пользователя или пароль')
     return redirect(url_for('user.login'))
